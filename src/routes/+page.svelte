2b2ca7ecc34ac05120c2e885a8272e1a0185b514
caiye <caiye2531889700@gmail.com>
2024-07-07 17:51:11 +0800

    setting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,13 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import NavCby from '@src/routes/NavCby.svelte';
+	import '$lib/styles/style.scss';
+</script>
+
+<div class="top-banner">
+	<img src="images/banner_main.jpg" alt="常备药标题" />
+	
+    <NavCby/>
+</div>
+
+<style lang="scss">
+</style>
