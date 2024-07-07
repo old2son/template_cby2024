7962121cce7022683194dd243fdb6cf369e4b842
caiye <caiye@2531889700@gmail.com>
2024-07-07 17:51:11 +0800

    setting

---
+++ src/routes/NavCby.svelte
@@ -0,0 +1,20 @@
+<div class="nav-wrap fixed">
+	<dl>
+		<dt class="js-nav-tabs">
+			<a href="#home" class="on">专题首页</a>
+			<a href="#hdjs">活动介绍</a>
+			<a href="#cbyb">常备药榜</a>
+			<a href="#bjsp">保健食品榜</a>
+			<a href="#ylqx">医疗器械及消毒用品榜</a>
+			<a href="#swhz">商务合作</a>
+			<a href="#mtzy">媒体资源</a>
+		</dt>
+		<dd>
+			<a href="/signup" target="_blank">申报通道</a>
+		</dd>
+	</dl>
+</div>
+
+<style lang="scss">
+	
+</style>
